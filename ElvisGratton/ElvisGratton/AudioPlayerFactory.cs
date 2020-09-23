70fe1502bf3fb3b4732bb4c594d8c736a713686c
Maxime Beauvais-St-Pierre <max.bsp@outlook.com>
2020-09-22 22:02:33 -0400

    Premier commit.

---
+++ ElvisGratton/ElvisGratton/AudioPlayerFactory.cs
@@ -0,0 +1,19 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElvisGratton
+{
+    public class AudioPlayerFactory
+    {
+        static public ISimpleAudioPlayer Create(string filename)
+        {
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+
+            player.Load($"{filename}.mp3");
+
+            return player;
+        }
+    }
+}
---
+++ ElvisGratton/ElvisGratton/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ElvisGratton
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            PlayCommand = new Command<string>(filename =>
+            {
+                var player = AudioPlayerFactory.Create(filename);
+                player.Play();
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand PlayCommand { private set; get; }
+
+    }
+}
